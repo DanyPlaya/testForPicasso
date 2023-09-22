34f2969150d247d126cdeceb92d5b1f7a47e09b9
DanyPlaya <danila.lusuy@gmail.com>
2023-09-22 16:37:27 +0300

    feat: create reduxStore

---
+++ src/app/providers/storeProvider/config/store.ts
@@ -0,0 +1,16 @@
+import { baseApi } from "@/shared/api";
+import { StateSchema } from "./stateSchema";
+import { ReducersMapObject, configureStore } from '@reduxjs/toolkit'
+
+
+export function createStore() {
+    const rootReducers: ReducersMapObject<StateSchema> = {
+        [baseApi.reducerPath]: baseApi.reducer,
+    }
+    const store = configureStore({
+        reducer: rootReducers,
+        middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(baseApi.middleware)
+    })
+    return store
+}
+export type AppDispatch = ReturnType <typeof createStore>['dispatch']
