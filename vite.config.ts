9774ca2d9c2bb096e7a9a95d77bf53e0e8e41c7b
DanyPlaya <danila.lusuy@gmail.com>
2023-09-23 15:42:31 +0300

    fix:base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import path from 'path'
 import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/testForPicasso',
+  
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
