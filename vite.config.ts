4ffad8fe82a4102c386af634948301aeddb2561c
DanyPlaya <danila.lusuy@gmail.com>
2023-09-23 15:22:40 +0300

    fix: test

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,6 @@ export default defineConfig({
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
-  base:'./testForPicasso'
+  base:'/testForPicasso'
   
 })
