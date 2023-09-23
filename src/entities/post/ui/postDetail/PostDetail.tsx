a0aed8db30b22f3dd4ee054486c967dad3b5cd6f
DanyPlaya <danila.lusuy@gmail.com>
2023-09-23 12:22:13 +0300

    feat!: create PostDetail commponent

---
+++ src/entities/post/ui/postDetail/PostDetail.tsx
@@ -0,0 +1,53 @@
+import { getRouteMain } from "@/shared/consts/router";
+import {
+  Box,
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Heading,
+  Stack,
+  StackDivider,
+  Text,
+} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+type PostItemProps = {
+    id?:number,
+    body?:string,
+    title?:string,
+   
+  }
+export const PostDetail = (props:PostItemProps) => {
+  const {body,id,title} = props
+  return (
+    <Card maxW="300px">
+      {" "}
+      {/* Установите максимальную ширину */}
+      <CardHeader>
+        <Heading size="md">Post №{id}</Heading>
+      </CardHeader>
+      <CardBody>
+        <Stack divider={<StackDivider />} spacing="4">
+          <Box>
+            <Heading size="xs" textTransform="uppercase">
+              {title}
+            </Heading>
+            <Text
+              maxW="100%"
+              maxH="100px"
+              pt="2"
+              fontSize="sm"
+            >
+              {body}
+            </Text>
+          </Box>
+          <Box>
+            <Link to={getRouteMain()}>
+              <Button>Back</Button>
+            </Link>
+          </Box>
+        </Stack>
+      </CardBody>
+    </Card>
+  );
+};
