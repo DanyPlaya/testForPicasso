d939fadf6e14db98c8758f1f629de0937c6d3cf9
DanyPlaya <danila.lusuy@gmail.com>
2023-09-23 12:24:44 +0300

    feat: export file

---
+++ src/entities/post/index.ts
@@ -0,0 +1,4 @@
+export {postApi} from './api/postApi'
+export {PostDetail} from './ui/postDetail/PostDetail'
+export {PostDetailSkeleton} from './ui/postDetail/PostDetailSkeleton'
+export {PostItem} from './ui/postItem/PostItem'
---
+++ src/features/postDetailById/index.ts
@@ -0,0 +1,1 @@
+export {PostDetailById} from './ui/PostDetaiById'
